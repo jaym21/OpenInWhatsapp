e178397b3acc2cbee34862b1e1cc87fee33899a4
jaym21 <jaynmistry21@gmail.com>
2021-04-13 18:21:11 +0530

    added theme

--- app/src/main/java/dev/jaym/openinwhatsapp/MainActivity.kt
+++ app/src/main/java/dev/jaym/openinwhatsapp/MainActivity.kt
@@ -1,20 +1,22 @@
 package dev.jaym.openinwhatsapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.text.isDigitsOnly
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var number: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //initializing a variable for storing text from intent
+        var number: String = "0"
 
         //checking if the action of intent received is PROCESS_TEXT
         if (intent.action == Intent.ACTION_PROCESS_TEXT) {
@@ -37,26 +39,40 @@ class MainActivity : AppCompatActivity() {
         intentWhatsApp.setPackage("com.whatsapp")
         //as per whatsapp api the number should of format 919087654321(i.e country code and number only)
         //first checking if the selected number has a + in it
-        if (number[0] == '+') {
+        val data: String = if (number[0] == '+') {
             //if there is + then removing it by taking a substring from 1 to last character
             number.substring(1)
         }else if (number.length == 10) {
             //if the number length is 10 that means the selected text only has number and country code is not present
             //adding the country code to the number
             "91$number"
+        }else {
+            number
         }
 
         //setting the data for intent
         //making use of whatsapp api format
-        intentWhatsApp.data = Uri.parse("https://wa.me/$number")
+        intentWhatsApp.data = Uri.parse("https://wa.me/$data")
 
         //checking if whatsapp is available on device
-        if (packageManager.resolveActivity(intentWhatsApp, 0) != null) {
+//        if (isWhatsAppAvailable()) {
             //if it is present then starting the intent to opening number in whatsapp
             startActivity(intentWhatsApp)
-        }else {
-            //if it is not present on device
-            Toast.makeText(this, "WhatsApp not available on device", Toast.LENGTH_LONG).show()
-        }
+//        }else {
+//            //if it is not present on device
+//            Toast.makeText(this, "WhatsApp not available on device", Toast.LENGTH_LONG).show()
+//        }
     }
+//
+//    private fun isWhatsAppAvailable(): Boolean {
+//    var isAvailable: Boolean
+//    try {
+//            packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
+//            isAvailable = true
+//        }catch (e: PackageManager.NameNotFoundException) {
+//            isAvailable = false
+//        }
+//
+//    return isAvailable
+//    }
 }
