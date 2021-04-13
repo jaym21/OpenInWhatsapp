6d1abb09f3c27a0e6174e4ffd3ef5ac725e949ee
jaym21 <jaynmistry21@gmail.com>
2021-04-13 13:42:35 +0530

    added an intent to open the number in whatsapp

--- app/src/main/java/dev/jaym/openinwhatsapp/MainActivity.kt
+++ app/src/main/java/dev/jaym/openinwhatsapp/MainActivity.kt
@@ -32,15 +32,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun openWhatsApp(number: String) {
         //making an intent to open whatsapp
-        val intent = Intent(Intent.ACTION_VIEW)
+        val intentWhatsApp = Intent(Intent.ACTION_VIEW)
         //opening using whatsapp api
-        intent.setPackage("com.whatsapp")
+        intentWhatsApp.setPackage("com.whatsapp")
         //as per whatsapp api the number should of format 919087654321(i.e country code and number only)
         //first checking if the selected number has a + in it
         if (number[0] == '+') {
             //if there is + then removing it by taking a substring from 1 to last character
             number.substring(1)
-        } else if (number.length == 10) {
+        }else if (number.length == 10) {
             //if the number length is 10 that means the selected text only has number and country code is not present
             //adding the country code to the number
             "91$number"
@@ -48,6 +48,15 @@ class MainActivity : AppCompatActivity() {
 
         //setting the data for intent
         //making use of whatsapp api format
-        intent.data = Uri.parse("https://wa.me/$number")
+        intentWhatsApp.data = Uri.parse("https://wa.me/$number")
+
+        //checking if whatsapp is available on device
+        if (packageManager.resolveActivity(intentWhatsApp, 0) != null) {
+            //if it is present then starting the intent to opening number in whatsapp
+            startActivity(intentWhatsApp)
+        }else {
+            //if it is not present on device
+            Toast.makeText(this, "WhatsApp not available on device", Toast.LENGTH_LONG).show()
+        }
     }
 }
